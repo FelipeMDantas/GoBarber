78dd54eaea0cc87ec03af4972ea05c26cda46fbb
FelipeMDantas <felipemdantas@terra.com.br>
2022-03-09 21:24:51 -0300

    Docker Postgre image database insersion via Sequelize ORM.

---
+++ BackEnd/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
