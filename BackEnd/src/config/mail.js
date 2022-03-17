982209a67b2914cc7448ba00883852d11c6c4a81
FelipeMDantas <felipemdantas@terra.com.br>
2022-03-16 22:59:26 -0300

    Sending, listing, and marking as read regarding notifications, appointment canceling and Nodemailer configured.

---
+++ BackEnd/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'a444af4e06be64',
+    pass: '318eb1bc3b8587',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
