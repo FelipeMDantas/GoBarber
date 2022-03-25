7ce6079c73b0cc197788f91285892ae5ee26bcfc
FelipeMDantas <felipemdantas@terra.com.br>
2022-03-24 22:59:57 -0300

    Routes and Reactotron configured.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,14 @@
+import { Router } from "react-router-dom";
+import Routes from "./routes";
+import history from "./services/history";
+import './config/ReactotronConfig';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
 }
 
 export default App;
