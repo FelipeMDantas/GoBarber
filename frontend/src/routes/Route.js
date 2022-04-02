2048f8f7ccbf3a6917c0ea3ec5269811c276a20b
FelipeMDantas <felipemdantas@terra.com.br>
2022-04-01 23:17:43 -0300

    Authentication persistence configured.

--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import { Route, Redirect } from "react-router-dom";
 import AuthLayout from "~/pages/_layouts/auth";
 import DefaultLayout from "~/pages/_layouts/default";
-import store from '~/store';
+import { store } from '~/store';
 
 export default function RouteWrapper({
   component: Component,
@@ -10,6 +10,8 @@ export default function RouteWrapper({
   ...rest
 }) {
   const signed = store.getState().auth.signed;
+  console.tron.log('Signed: ' + Boolean(signed))
+  console.tron.log('isPrivate: ' + Boolean(isPrivate))
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
--- frontend/src/store/modules/user/reducer.js
+++ frontend/src/store/modules/user/reducer.js
@@ -1,17 +1,14 @@
 import produce from "immer";
 
 const INITAL_STATE = {
-  token: null,
-  signed: null,
-  loading: false,
+  profile: null,
 };
 
-export default function auth(state = INITAL_STATE, action) {
+export default function user(state = INITAL_STATE, action) {
   switch (action.type) {
     case "@auth/SIGN_IN_SUCCESS":
       return produce(state, (draft) => {
-        draft.token = action.payload.token;
-        draft.signed = true;
+        draft.profile = action.payload.user;
       });
     default:
       return state;
@@ -19,17 +16,15 @@ export default function auth(state = INITAL_STATE, action) {
 }
 
 /*export default function auth(state = INITAL_STATE, action) {
-  console.tron.log({ action })
   return produce(state, (draft) => {
     switch (action.type) {
       case "@auth/SIGN_IN_SUCCESS": {
-        draft.token = action.payload.token;
-        draft.signed = true;
-        draft.loading = false;
+        draft.profile = action.payload.user;
         break;
       }
 
       default:
     }
   });
-}*/
+}
+*/
