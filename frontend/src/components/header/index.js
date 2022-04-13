9f775bb8e3a27c096855fe3ab2e935ebbb3f06f4
FelipeMDantas <felipemdantas@terra.com.br>
2022-04-12 23:04:41 -0300

    Profile picture functionalities established and logout button operative.

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -1,9 +1,12 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import Notifications from '~/components/Notifications'
+import Notifications from "~/components/Notifications";
 import logo from "~/assets/logo-purple.svg";
 import { Container, Content, Profile } from "./styles";
 
 export default function Header() {
+  const profile = useSelector((state) => state.user.profile);
+  
   return (
     <Container>
       <Content>
@@ -16,11 +19,14 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Felipe</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
+              src={
+                profile.avatar.url ||
+                "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
+              }
               alt="Felipe"
             />
           </Profile>
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { Form, Input } from "@rocketseat/unform";
+import { signOut } from "~/store/modules/auth/actions";
 import { updateProfileRequest } from "~/store/modules/user/actions";
 import AvatarInput from "./AvatarInput";
 import { Container } from "./styles";
@@ -10,6 +11,10 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut())
   }
 
   return (
@@ -36,7 +41,7 @@ export default function Profile() {
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>Sair do GoBarber</button>
     </Container>
   );
 }
