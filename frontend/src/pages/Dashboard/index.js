b98108945aac2cb9369989f5d21ceb482ffa64f9
FelipeMDantas <felipemdantas@terra.com.br>
2022-04-14 23:03:31 -0300

    Dashboard stylization completed.

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,6 +1,6 @@
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import api from "~/services/api";
-import { Container } from "./styles";
+import { Container, Time } from "./styles";
 
 export default function Dashboard() {
   return (
@@ -14,6 +14,25 @@ export default function Dashboard() {
           <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Felipe</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Felipe</span>
+        </Time>
+        <Time available>
+          <strong>11:00</strong>
+          <span>Felipe</span>
+        </Time>
+      </ul>
     </Container>
   );
 }
