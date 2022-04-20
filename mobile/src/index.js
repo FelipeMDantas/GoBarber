d4b0a766455fd64426d19f50d4aab0327e0a1b6f
FelipeMDantas <felipemdantas@terra.com.br>
2022-04-20 20:25:34 -0300

    Mobile sign up and sign in pages stylized.

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,12 @@
-import {View} from 'react-native';
-import Teste from '~/Teste';
+import React from 'react';
+import {StatusBar} from 'react-native';
+import Routes from './routes';
 
 export default function App() {
-  return <Teste />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 }
---
+++ mobile/src/pages/SignUp/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Image} from 'react-native';
+import logo from '~/assets/logo.png';
+import Background from '~/components/Background';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({navigation}) {
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCaptalize="none"
+            placeholder="Nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCaptalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+
+          <SubmitButton>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
+}
