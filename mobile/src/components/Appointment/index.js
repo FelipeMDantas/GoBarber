f2c892e910230e5d73c3535c3c34af7aeeccba00
FelipeMDantas <felipemdantas@terra.com.br>
2022-04-23 21:47:06 -0300

    Routes defined and stylized. Appointments data retrieved via API and subsequently listed.

---
+++ mobile/src/components/Appointment/index.js
@@ -0,0 +1,35 @@
+import React, {useMemo} from 'react';
+import {parseISO, formatRelative} from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Container, Left, Avatar, Info, Name, Time} from './styles';
+
+export default function Appointment({data}) {
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(data.date), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [data.date]);
+  return (
+    <Container past={data.past}>
+      <Left>
+        <Avatar
+          source={{uri: data.provider.avatar ? data.provider.avatar.url : null}}
+        />
+
+        <Info>
+          <Name>{data.provider.name}</Name>
+          <Time>{dateParsed}</Time>
+        </Info>
+      </Left>
+
+      {data.cancelable ? (
+        <TouchableOpacity onPress={() => {}}>
+          <Icon name="event-busy" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      ) : null}
+    </Container>
+  );
+}
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,11 +1,35 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    SignIn,
-    SignUp,
-  }),
-);
+export default (isSignedIn = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator({
+          Dashboard,
+          Profile,
+        }),
+      },
+      /*{
+        initialRouteName: isSignedIn ? 'App' : 'Sign',
+      },*/
+      {
+        tabBarOptions: {
+          KeyboardHidesTabBar: true,
+          activeTintColor: '#FFF',
+          inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+          style: {
+            backgroundColor: '#8d41a8',
+          },
+        },
+      },
+    ),
+  );
