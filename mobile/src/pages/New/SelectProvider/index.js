29633bd36e0781081babe5f94b3b910129561315
FelipeMDantas <felipemdantas@terra.com.br>
2022-04-25 14:31:43 -0300

    Providers listing, dynamic DatePicker - depending on the used mobile OS -, and schedule selection finished.

--- mobile/src/pages/New/SelectProvider/index.js
+++ mobile/src/pages/New/SelectProvider/index.js
@@ -3,9 +3,9 @@ import {TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '~/services/api';
 import Background from '~/components/Background';
-import {Container} from './styles';
+import {Container, ProvidersList, Provider, Avatar, Name} from './styles';
 
-export default function SelectProvider() {
+export default function SelectProvider({navigation}) {
   const [providers, setProviders] = useState([]);
 
   useEffect(() => {
@@ -20,7 +20,21 @@ export default function SelectProvider() {
 
   return (
     <Background>
-      <Container></Container>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({item: provider}) => (
+            <Provider
+              onPress={() => navigation.navigate('SelectDateTime', {provider})}>
+              <Avatar
+                source={{uri: provider.avatar ? provider.avatar.url : null}}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
     </Background>
   );
 }
